e8b7c936467343042d2f7db85955abef58674b94
indrol12 <indrolofficial@gmail.com>
2024-04-22 11:38:52 +0530

    area file committed

---
+++ area.java
@@ -0,0 +1,16 @@
+public class javademo{
+
+public static void main(String args[]){
+
+area_of_rectangle();
+
+
+}
+static void area_of_rectangle()
+{
+System.out.println("area_of_rectangle is:"+(8*4));
+}
+
+
+
+}
